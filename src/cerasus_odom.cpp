8d0d0573313e1b0b8cf6b6d289d0beaa250dcd05
xiabinyu <xiabinyu@sjtu.edu.cn>
2019-04-08 03:37:11 -0700

    hi

--- src/cerasus_odom.cpp
+++ src/cerasus_odom.cpp
@@ -1,4 +1,13 @@
 //
 // Created by xiabi on 2019/4/8.
 //
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <turtlesim/Pose.h>
 
+int main(int argc, char** argv){
+	ros::init(argc, argv, "cerasus_odom")
+
+	ros::NodeHandle node;
+
+}
---
+++ src/cerasus_odom_class.h
@@ -0,0 +1,1 @@
+//Class Head file

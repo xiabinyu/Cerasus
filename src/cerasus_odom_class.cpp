c81f56c7e2034727d51fcd45f081026d989cb877
Binyu Xia_Laptop <xiabinyu@sjtu.edu.cn>
2019-04-09 18:35:54 +0800

    tuned

--- src/cerasus_odom_class.cpp
+++ src/cerasus_odom_class.cpp
@@ -2,7 +2,7 @@
 //F(x,y)=(x*cos(a)-y*sin(a),x*sin(a)+y*cos(a))
 #include "cerasus_odom_class.h"
 #include <math.h>
-CerasusOdom::CerasusOdom(tf::Transform _position,float _cycle=0){
+CerasusOdom::CerasusOdom(tf::Transform _position,float _cycle=0.01){
     CYCLE=_cycle;
     Odom=_position;
 };
@@ -15,13 +15,19 @@ geometry_msgs::Vector3 CerasusOdom::TransAc(double x,double y){
     return NewAc;
 };
 tf::Transform CerasusOdom::OdomUpdate(sensor_msgs::Imu _imu) {
-    Angle+=_imu.angular_velocity.z;
+    Angle+=_imu.angular_velocity.z*CYCLE;
     Acceleration=TransAc(_imu.linear_acceleration.x,_imu.linear_acceleration.y);
-    Velocity.x+=Acceleration.x;
-    Velocity.y+=Acceleration.y;
-    Velocity.z+=Acceleration.z;
+    Velocity.x+=Acceleration.x*CYCLE;
+    Velocity.y+=Acceleration.y*CYCLE;
+    Velocity.z+=Acceleration.z*CYCLE;
     Position.x+=Velocity.x;
     Position.y+=Velocity.y;
     Position.z+=Velocity.z;
+    tf::Transform temp_transform;
+    temp_transform.setOrigin(tf::Vector3(Position.x,Position.y,Position.z));//设定坐标
+    tf::Quaternion temp_quaternion;
+    temp_quaternion.setRPY(0, 0, Angle);//设定方向
+    temp_transform.setRotation(temp_quaternion);
+    return temp_transform;
 }
 void CerasusOdom::EndOdom(){};

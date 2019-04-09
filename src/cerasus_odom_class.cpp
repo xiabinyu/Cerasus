cf7a595bb3b2bb8865df9ca3ee4d500c9ce59ca3
Binyu Xia_Laptop <xiabinyu@sjtu.edu.cn>
2019-04-09 18:25:49 +0800

    tuned

--- src/cerasus_odom_class.cpp
+++ src/cerasus_odom_class.cpp
@@ -1,7 +1,27 @@
 // Class Source
+//F(x,y)=(x*cos(a)-y*sin(a),x*sin(a)+y*cos(a))
 #include "cerasus_odom_class.h"
-
-CerasusOdom::CerasusOdom(tf::Transform _position){
-
+#include <math.h>
+CerasusOdom::CerasusOdom(tf::Transform _position,float _cycle=0){
+    CYCLE=_cycle;
+    Odom=_position;
 };
+
+geometry_msgs::Vector3 CerasusOdom::TransAc(double x,double y){
+    geometry_msgs::Vector3 NewAc;
+    NewAc.x=x*cosf(Angle)-y*sinf(Angle);
+    NewAc.y=x*sinf(Angle)+y*cosf(Angle);
+    NewAc.z=0;
+    return NewAc;
+};
+tf::Transform CerasusOdom::OdomUpdate(sensor_msgs::Imu _imu) {
+    Angle+=_imu.angular_velocity.z;
+    Acceleration=TransAc(_imu.linear_acceleration.x,_imu.linear_acceleration.y);
+    Velocity.x+=Acceleration.x;
+    Velocity.y+=Acceleration.y;
+    Velocity.z+=Acceleration.z;
+    Position.x+=Velocity.x;
+    Position.y+=Velocity.y;
+    Position.z+=Velocity.z;
+}
 void CerasusOdom::EndOdom(){};
--- src/cerasus_odom_class.h
+++ src/cerasus_odom_class.h
@@ -3,14 +3,20 @@
 
 #include <tf/transform_broadcaster.h>
 #include <geometry_msgs/Vector3.h>
+#include <sensor_msgs/Imu.h>
 class CerasusOdom{
 public:
-    CerasusOdom(tf::Transform _position);
+    CerasusOdom(tf::Transform _position,float _cycle=0);
+    tf::Transform OdomUpdate(sensor_msgs::Imu _imu);
     void EndOdom();
 
 private:
+    float Angle;
+    geometry_msgs::Vector3 TransAc(double x,double y);
     tf::Transform Odom;
     geometry_msgs::Vector3 Position;
     geometry_msgs::Vector3 Velocity;
+    geometry_msgs::Vector3 Acceleration;
+    float CYCLE;
 
 };

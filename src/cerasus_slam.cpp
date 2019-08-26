2ce8ad5fabcc6fa7e18f9820ee07fb223c34f7a8
Binyu Xia_Laptop <xiabinyu@sjtu.edu.cn>
2019-08-26 11:44:35 +0800

    add slam

--- src/cerasus_slam.cpp
+++ src/cerasus_slam.cpp
@@ -1,15 +1,59 @@
-//
-// Created by xiabinyu on 7/16/19.
-//
-
+#define CONST_LIDARMIN 0.1
+#define CONST_LIDARMAX 1.0
+#define CONST_ZERO_ANG 180
+#define CONST_MAX_ANG 100
+#define CONST_LANDMARK_GATE 0.5
+#define CONST_ROAD_GATE 2.5
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
 #include <turtlesim/Pose.h>
 #include <sensor_msgs/Imu.h>
 #include <sensor_msgs/LaserScan.h>
 #include "cerasus_odom_class.h"
+#include <cmath.h>
 //#include "cerasus_odom_class.h"
 
+struct Dot_a{
+    double X;
+    double Y;
+};
+class LandMark{
+private:
+    Dot_a *Dot;
+    Dot_a LandMark_Dot;
+    int DotNum;
+    double SqrDis(Dot_a d1,Dot_a d2){
+        return (d1.X-d2.X)*(d1.X-d2.X)+(d1.Y-d2.Y)*(d1.Y-d2.Y);
+    }
+    void Update(){
+        LandMark_Dot.X+=Dot[DotNum].X;
+        LandMark_Dot.Y+=Dot[DotNum].Y;
+    }
+public:
+    LandMark(){
+        Dot= new Dot_a(50);
+        DotNum=0;
+        LandMark_Dot={0,0};
+    }
+    int insertDot(Dot_a tdot){
+        if(DotNum==0){
+            Dot[DotNum]=tdot;
+            DotNum++;
+            Update();
+            return 0;
+        }
+        if(SqrDis(Dot[0],tdot)<=CONST_LANDMARK_GATE*CONST_LANDMARK_GATE){
+            Dot[DotNum]=tdot;
+            DotNum++;
+            Update();
+            return 0;
+        } else if(SqrDis(Dot[0],tdot)<=CONST_ROAD_GATE*CONST_ROAD_GATE){
+            return 1;
+        }
+        return -1;
+    }
+
+};
 
 void Callback(sensor_msgs::LaserScan scan);
 
@@ -26,17 +70,13 @@ int main(int argc, char** argv){
 }
 
 void Callback(sensor_msgs::LaserScan scan) {
+    Dot_a Data[360];
+    double* LandMark[180];
+    int count=0;
+    for(int i=0;i<360;i++){
+        if(lidar.ranges[i]>=CONST_LIDARMIN&&lidar.ranges[i]<=CONST_LIDARMAX){
+            Data[count].X=lidar.ranges[i]*;
+        }
+    }
 
-    scan.header.stamp=ros::Time::now();
-    static ros::Publisher root_pub = nhp->advertise<sensor_msgs::LaserScan>("/laser_scan", 1000);//Root Command
-    root_pub.publish(scan);
-    //int Total=2*M_PI/scan.angle_increment;
-  //  int Total=360;
-  //  float z[720];
-  //  //int z_size[2]={2,Total};
-  //  for (unsigned int i=0;i<Total;i++){
-  //      z[0+(i<<1u)]=scan.ranges[i];
-  //      z[1+(i<<1u)]=i*scan.angle_increment;
-  //  }
-  //
 }
